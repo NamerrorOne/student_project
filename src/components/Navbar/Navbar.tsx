dbdd433f3d2485517ea69d42f7e8a03888cfbae9
Igor <igor14abramov@gmail.com>
2025-08-21 14:40:48 +0300

    films onClick  to my films

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -14,7 +14,7 @@ export const Navbar: FC<INavbarProps> = ({ onClick }) => {
       <nav className={styles.navbar}>
         <NavbarItem href="/" text="Search films" isActive />
         <NavbarItem
-          href="/films"
+          href="/favorites"
           text="My films"
           icon={<ValueIndicatorBadge value={7} />}
         />
